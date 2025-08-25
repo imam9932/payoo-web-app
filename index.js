9724d753b7fd8f24a69d1d0666056bb82c144025
imam9932 <imamhossan9932@gmail.com>
2025-08-25 12:38:14 +0600

    added update items

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // login button functionality
 document.getElementById('btn-login').addEventListener('click',function(event){
- const mobileNumber=12345678910
+ const mobileNumber=17188099320
  const pinNumber=1234
 
  const mobileNumberValue=document.getElementById('mobile-number').value ;
@@ -10,11 +10,21 @@ document.getElementById('btn-login').addEventListener('click',function(event){
  const pinNumberValueConverted=parseInt(pinNumberValue);
 
 //  console.log(mobileNumberValueConverted,pinNumberValueConverted)
+ 
 
  if(mobileNumberValueConverted===mobileNumber && pinNumberValueConverted===pinNumber){
   window.location.href='./home.html'
  }
  else{
-  alert('Invalid credentials')
+  alert('invalid information')
  }
+ 
 })
+// if(mobileNumberValueConverted!==mobileNumber){
+//   alert('mobile number invalid')
+//   return;
+// }
+// if(pinNumberValueConverted!==pinNumber){
+//   alert('Incorrect pin')
+//   return;
+// }
